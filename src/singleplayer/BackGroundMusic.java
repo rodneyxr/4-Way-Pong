74d76ae2ff4db29d5aad7293c5753883dc91d56b
Rodney Rodriguez <rodneyxr@gmail.com>
2016-06-29 09:52:35 -0500

    Update BackGroundMusic.java to work with Java 8

--- src/singleplayer/BackGroundMusic.java
+++ src/singleplayer/BackGroundMusic.java
@@ -1,12 +1,15 @@
 package singleplayer;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 
-import sun.audio.AudioPlayer;
-import sun.audio.AudioStream;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 /**
  * this class will handle the background music for the game
@@ -14,7 +17,6 @@ import sun.audio.AudioStream;
  * @author Rodney
  *
  */
-@SuppressWarnings("restriction")
 public class BackGroundMusic implements Runnable {
 	private Thread t;
 	private String threadName;
@@ -32,26 +34,24 @@ public class BackGroundMusic implements Runnable {
 	 * starts the thread for the background music
 	 */
 	public void run() {
-
-		InputStream in = null;
-		AudioStream as = null;
+		File audioFile = new File("Afterglow.wav");
 		try {
-			in = new FileInputStream("Afterglow.wav");
-			as = new AudioStream(in);
-		} catch (FileNotFoundException e) {
+			AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
+			AudioFormat format = audioStream.getFormat();
+			DataLine.Info info = new DataLine.Info(Clip.class, format);
+			Clip audioClip = (Clip) AudioSystem.getLine(info);
+			audioClip.open(audioStream);
+			audioClip.loop(Clip.LOOP_CONTINUOUSLY);
+		} catch (UnsupportedAudioFileException e) {
+			System.out.println("The specified audio file is not supported.");
 			e.printStackTrace();
 		} catch (IOException e) {
+			System.out.println("Error playing the audio file.");
+			e.printStackTrace();
+		} catch (LineUnavailableException e) {
+			System.out.println("Audio line for playing back is unavailable.");
 			e.printStackTrace();
 		}
-
-		AudioPlayer.player.start(as);
-		try {
-			Thread.sleep(300000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		AudioPlayer.player.stop(as);
 	}
 
 	/**
